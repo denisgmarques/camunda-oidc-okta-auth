0eb06d4d94a8f5dba656d7ef35577537a51690e1
Denis Giovan Marques <denis.marques@icolabora.com.br>
2021-05-05 17:12:08 -0300

    Fix - Logout url with id_token_hint

--- src/main/java/br/com/bpms/security/CustomLogoutSuccessHandler.java
+++ src/main/java/br/com/bpms/security/CustomLogoutSuccessHandler.java
@@ -1,7 +1,10 @@
 package br.com.bpms.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -15,9 +18,13 @@ import java.io.IOException;
 @Component
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
-    @Value("${okta.issuer.url}")
-    private String issuerUrl;
-        private static final String SIGNOUT_URL = "/login/admin/signout";
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+
+    private static final String SIGNOUT_URL = "/v1/logout";
+
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
 
     private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
@@ -26,10 +33,16 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
                                 final HttpServletResponse response,
                                 final Authentication authentication) throws IOException {
         final HttpSession session;
-        // ?id_token_hint=${id_token}&post_logout_redirect_uri=${post_logout_redirect_uri}&state=${state}
+
         if ((session = request.getSession(false)) != null) {
             session.invalidate();
         }
-        redirectStrategy.sendRedirect(request, response, issuerUrl + SIGNOUT_URL);
+
+        // https://dev-XPTO1231.okta.com/oauth2/default/v1/logout?id_token_hint=${id_token}&post_logout_redirect_uri=${post_logout_redirect_uri}&state=${state}
+        redirectStrategy.sendRedirect(request, response, issuer + SIGNOUT_URL + "?id_token_hint=" + getToken(authentication));
+    }
+
+    private String getToken(Authentication authentication) {
+        return ((OidcUser) authentication.getPrincipal()).getIdToken().getTokenValue();
     }
 }
